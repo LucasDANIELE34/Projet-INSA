f947b42525843e98494deb1a292f0d70bd42a082
LucasDANIELE34 <59765704+LucasDANIELE34@users.noreply.github.com>
2020-03-22 18:13:11 +0100

    possibilité d'ouvrir map avec 'telechargerMap()'

--- creation map/var.js
+++ creation map/var.js
@@ -2,6 +2,7 @@ document.getElementById('o_canvas').addEventListener("click", majPositionSouris)
 
 var canvas = document.getElementById('o_canvas');
 canvas = canvas.getContext('2d');//notre fenetre
+var urlServeur="https://tipne.000webhostapp.com/";
 
 var decor = [];//tableau contenant tous les objets à afficher du décor
 for (var i = 0; i < 20; i++) {
@@ -220,4 +221,40 @@ function resultatMap(){
     }
   }
   document.getElementById('resultatMap').value=JSON.stringify(decorAlegee);
+}
+
+function telechargerMap(cheminMap) {
+  var arguments = "chemin="+cheminMap;
+  var donnees;
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", urlServeur+"recuperer.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(arguments);
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      donnees = JSON.parse(this.responseText);
+      chargerMap(donnees);
+    }
+  };
+}
+
+function chargerMap(map){
+  for (var i = 1; i < map.length; i++) {
+    decor[map[i].i][map[i].j] = new texture(map[i].name,map[i].i,map[i].j,map[i].orientation,map[i].variante,map[i].texte,map[i].map);
+    if (map[i].name == 'maisonPorte') {
+      decor[map[i].i][map[i].j].iSortie = map[i].iSortie;
+      decor[map[i].i][map[i].j].jSortie = map[i].jSortie;
+    }
+
+    if (map[i].texte!=null) {
+      decor[map[i].i][map[i].j].phrases = decouperTexte(map[i].texte);
+    }
+    if (map[i].map!=null) {
+      decor[map[i].i][map[i].j].map = map[i].map;
+      decor[map[i].i][map[i].j].iSortie = map[i].iSortie;
+      decor[map[i].i][map[i].j].jSortie = map[i].jSortie;
+    }
+  }
 }
